3d98000507754ed50f33ea4fc8d539bde4b034e7
fidansamet <fidansamet@hacettepe.edu.tr>
2020-03-01 23:09:34 +0300

    Interests added

--- js/resume.js
+++ js/resume.js
@@ -26,3 +26,33 @@
   });
 
 })(jQuery); // End of use strict
+
+var slideIndex = 1;
+
+(function (document, window, index) {
+  showSlides(slideIndex);
+}(document, window, 0));
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("photos");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
